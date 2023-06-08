41a0741621d88f0e74da59d3d4027c9db51dd9ee
Mert A <111873325+devmrt0@users.noreply.github.com>
2023-06-08 19:19:57 +0300

    Add files via upload

---
+++ Controller/LeadController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Client.Services;
+using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IFSConsole.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Entity = Microsoft.Xrm.Sdk.Entity;
+using Azure.Core;
+using Microsoft.IdentityModel.Protocols;
+
+namespace IFSConsole.Controller
+{
+    public class LeadController : ApiController
+    {
+        #region GET method
+        [BasicAuthentication]
+        public HttpResponseMessage Get()
+        {
+            LeadClass lead = new LeadClass();
+            Address addr = new Address();
+            lead.Address = addr;
+            return Request.CreateResponse(HttpStatusCode.Created, lead);
+        }
+        #endregion
+        #region POST method
+        [BasicAuthentication]
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage POST(LeadClass leadClass)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            CrmConnection con = CrmConnection.Parse(ConfigurationManager.ConnectionStrings["CRMConnectionString"].ConnectionString);
+            OrganizationService service = null;
+            service = new OrganizationService(con);
+            try
+            {
+                Entity lead = new Entity("lead");
+                lead["subject"] = leadClass?.Subject;
+                lead["firstname"] = leadClass?.FirstName;
+                lead["lastname"] = leadClass?.LastName;
+                lead["companyname"] = leadClass?.Company;
+                lead["websiteurl"] = leadClass?.Website;
+                lead["address1_line1"] = leadClass?.Address?.Address_Line1;
+                lead["address1_line2"] = leadClass?.Address?.Address_Line2;
+                lead["address1_line3"] = leadClass?.Address?.Address_Line3;
+                lead["address1_city"] = leadClass?.Address?.City;
+                lead["address1_stateorprovince"] = leadClass?.Address?.State;
+                lead["address1_postalcode"] = leadClass?.Address?.Postal_Code;
+                lead["address1_country"] = leadClass?.Address?.Country;
+                Guid guid = service.Create(lead);
+                leadClass.LeadId = guid.ToString();
+                return Request.CreateResponse(HttpStatusCode.Created, leadClass);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.OK, ex.Message);
+            }
+        }
+        #endregion
+    }
+}  
+
+
---
+++ Models/LeadClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IFSConsole.Models
+{
+    public class LeadClass
+    {
+        public string LeadId { get; set; }
+        public string Subject { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public string Website { get; set; }
+        public Address Address { get; set; }
+    }
+    public class Address
+    {
+        public string Address_Line1 { get; set; }
+        public string Address_Line2 { get; set; }
+        public string Address_Line3 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Postal_Code { get; set; }
+        public string Country { get; set; }
+    }
+}
